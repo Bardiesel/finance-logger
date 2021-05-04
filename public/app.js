1cc12cbbedb39dd0981e2d10781c18739a11148d
Bardiesel <bardiamarandi@hotmail.com>
2021-05-05 00:54:21 +0430

    init commit :zap:

---
+++ public/app.js
@@ -0,0 +1,25 @@
+import { Invoice } from './models/invoice.js';
+import { Payment } from './models/payment.js';
+import { listTemplate } from './models/listTemplate.js';
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new listTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+});
